c9fd1e4fd688c1d537b3c15982768951d2784cfe
Antoine <antoine.thenevin@gmail.com>
2020-10-06 12:07:34 +0200

    Init Cmake Project

---
+++ Voronoi/source/Voronoi.cpp
@@ -0,0 +1,12 @@
+﻿// Voronoi.cpp : définit le point d'entrée de l'application.
+//
+
+#include "../header/Voronoi.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
